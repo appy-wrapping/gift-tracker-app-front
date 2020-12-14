48cc6e1c3f9b6f61b0b18787143586e32f3fd522
Nelboh <eshobbs7@gmail.com>
2020-12-14 11:45:21 +0000

    Made the basic HTML structure for the list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <nav>
+      <h1>Gift Tracker App</h1>
+    </nav>
+
+    <h2>"Friend Name Here"</h2>
+
+    <h2>"Budget Here"</h2>
+
+    <table>
+      <tr>
+        <th></th>
+        <th></th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Bought?</th>
+      </tr>
+
+      {/* Stick a row component here */}
+      <tr>
+        <td>Icon1</td>
+        <td>Icon2</td>
+        <td>Description</td>
+        <td>Price</td>
+        <td>Bought?</td>
+      </tr>
+    </table>
+
+    <h2>Total Goes Here</h2>
+  </>
+
+
+);
 
 export default App;
