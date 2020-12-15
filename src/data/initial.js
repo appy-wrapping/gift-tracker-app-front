d2cd7b0332d4ccbf18d255da25b6b619ef57c92d
Nelboh <eshobbs7@gmail.com>
2020-12-15 09:44:34 +0000

    Made basic inputs that accept typed values for table row

---
+++ src/data/initial.js
@@ -0,0 +1,10 @@
+const initial = {
+    "item_name": "Your gift here",
+    "price": 0.0,
+    "bought": false,
+    "total": 0.0,
+    "friend_name": "Recipient's Name"
+};
+
+export default initial;
+
