7d79d08564eb11f091851f7ef626d6a40da91f1d
Nelboh <eshobbs7@gmail.com>
2020-12-16 12:18:59 +0000

    Added a save button for when editing is true

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,13 +1,13 @@
 // import react from "react";
 // import Inputs from "./Inputs";
 
-const Row = ({ item_name, price, bought }) => (
+const Row = ({ item_name, price, bought, editing }) => (
 	<>
-		<td className="tdNarrow"><button class="tableButtons deleteButton"></button></td>
-		<td className="tdNarrow"><button class="tableButtons editButton"></button></td>
-		<td class="tableInputs">{item_name}</td>
-		<td class="tableInputs">{price}</td>
-		<td class="tableInputs"><button className={bought ? "button tickButton" : "button crossButton"}></button></td>
+		<td className="tdNarrow"><button className="tableButtons deleteButton"></button></td>
+		<td className="tdNarrow"><button className={!editing ? "tableButtons editButton" : "button saveButton"}></button></td>
+		<td className="tableInputs">{item_name}</td>
+		<td className="tableInputs">{price}</td>
+		<td className="tableInputs"><button className={bought ? "button tickButton" : "button crossButton"}></button></td>
 	</>
 );
 
