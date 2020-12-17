8fd0b2c2f4bacfc41d2d4672683ba41fa56e5e27
Nelboh <eshobbs7@gmail.com>
2020-12-17 20:59:35 +0000

    Added an edit button to friend, fixed styling

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,8 +1,9 @@
 // import react from "react";
 const Friend = ({ friendName }) => (
-	<>
+	<div className="friendHolder">
 		<h2 className="card friendCard">{friendName}</h2>
-	</>
+		<button className="button editButtonTwo" />
+	</div>
 );
 
 export default Friend;
