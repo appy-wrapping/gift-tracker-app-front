1d1e4e90be5226bd76521017fecbd832ae875c7d
Tarik <tarikmerrylees@gmail.com>
2020-12-18 12:25:59 +0000

    added Total: label to the total component

--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -2,7 +2,7 @@ const Total = ({ priceSum, budget }) => {
 
     return (
         <>
-            <h2 className="footer" style={(priceSum > budget ? { color: "red" } : {})}>£{priceSum.toFixed(2)}</h2>
+            <h2 className="footer" style={(priceSum > budget ? { color: "red" } : {})}>Total: £{priceSum.toFixed(2)}</h2>
         </>
     );
 
