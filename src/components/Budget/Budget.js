8bb828f49965d868784a9baef6b48c11e1c54982
Matt Maslen <mattmaslen123@gmail.com>
2020-12-18 13:02:16 +0000

    added editing className to editing inputs on item name, price, friend name and budget

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,48 +1,54 @@
 import { useState } from "react";
 
 const Budget = ({ id, name, propBudget, editFriend }) => {
-    const [editingBudget, setEditingBudget] = useState(false);
+	const [editingBudget, setEditingBudget] = useState(false);
 
-    const [budget, setBudget] = useState(propBudget);
+	const [budget, setBudget] = useState(propBudget);
 
-    const handleEditingBudget = () => {
-        if (editingBudget) {
-            editFriend({
-                id: id,
-                name: name,
-                budget: budget
-            });
-        }
-        setEditingBudget(
-            !editingBudget
-        )
-    }
+	const handleEditingBudget = () => {
+		if (editingBudget) {
+			editFriend({
+				id: id,
+				name: name,
+				budget: budget,
+			});
+		}
+		setEditingBudget(!editingBudget);
+	};
 
-    const handleBudget = (e) => {
-        setBudget(
-            e.currentTarget.value
-        )
-    }
+	const handleBudget = (e) => {
+		setBudget(e.currentTarget.value);
+	};
 
-    return (
-        <>
-            {editingBudget ?
-                <div className="budgetHolder">
-                    <input type="number"
-                        onChange={handleBudget}
-                        className="card budgetCard"
-                        value={`${budget}`} />
+	return (
+		<>
+			{editingBudget ? (
+				<div className="budgetHolder">
+					<input
+						type="number"
+						onChange={handleBudget}
+						className="card budgetCard editing"
+						value={`${budget}`}
+					/>
 
-                    <button className="button editButtonTwo" id="pencil" onClick={handleEditingBudget}></button>
-                </div>
-                :
-                <div className="budgetHolder">
-                    <h2 className="card budgetCard">Budget: £{budget}</h2>
-                    <button className="button editButtonTwo" id="pencil" onClick={handleEditingBudget}></button>
-                </div>
-            }
-        </>
-    )
-}
+					<button
+						className="button editButtonTwo"
+						id="pencil"
+						onClick={handleEditingBudget}
+					></button>
+				</div>
+			) : (
+				<div className="budgetHolder">
+					<h2 className="card budgetCard">Budget: £{budget}</h2>
+					<button
+						className="button editButtonTwo"
+						id="pencil"
+						onClick={handleEditingBudget}
+					></button>
+				</div>
+			)}
+		</>
+	);
+};
 
 export default Budget;
--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -1,11 +1,14 @@
 const Total = ({ priceSum, budget }) => {
-
-    return (
-        <>
-            <h2 className="footer" style={(priceSum > budget ? { color: "red" } : {})}>£{priceSum.toFixed(2)}</h2>
-        </>
-    );
-
+	return (
+		<>
+			<h2
+				className="footer"
+				style={priceSum > budget ? { color: "#CC5B3D" } : {}}
+			>
+				£{priceSum.toFixed(2)}
+			</h2>
+		</>
+	);
 };
 
 export default Total;
